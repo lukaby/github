b4c14a605ebe89fd6b4e1d1831ba9e25bb686cab
lukaby <lukaby@.github.com>
2020-01-27 09:18:03 +0200

    index.html

---
+++ script.js
@@ -0,0 +1,16 @@
+function openNav(){
+	document.getElementById('menu-c').style.height='100%';
+}
+function closeNav(){
+	document.getElementById('menu-c').style.height='0px';
+}
+
+function openModal(){
+	document.getElementById('modall').style.height='100vh';
+	document.getElementById('modall').style.width='100%';
+}
+
+function closeModal(){
+	document.getElementById('modall').style.height='0vh';
+	document.getElementById('modall').style.width='0%';
+}
